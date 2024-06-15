48f5dceee066ae70c7c1d02568bd797f8b2ed48c
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-15 15:14:06 -0700

    Update example.c

--- example/src/example.c
+++ example/src/example.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 
 int* get_c_ptr() {
+    // Make memory allocation of 1 int
     int* ptr = (int*)malloc(sizeof(int));
-    // Check if memory allocation Failed
+    // Check if memory allocation failed
     int* val = (ptr != NULL) ? ptr : NULL;
     // Assign value to int* ptr
     *val = 12;
+    // Return ptr with assigned value
     return val;
 }
