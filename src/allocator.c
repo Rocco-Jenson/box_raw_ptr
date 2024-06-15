0efb09095bc9531715d0e549f26e95a6f63c1207
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-15 11:57:50 -0700

    Update allocator.c

--- src/allocator.c
+++ src/allocator.c
@@ -1,23 +1,15 @@
 /* Wrapper around malloc() and free() to remove libc dependency */
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* Check if architecture is x64 or ARM
-#if defined(__x86_64__) 
-    || defined(_M_X64) 
-    || defined(__aarch64)
+#if defined(__x86_64__) || defined(_M_X64) || defined(__aarch64)
     typedef uint64_t arch_type;
 /* Check if architecture is x86 or ARM x86 */
-#elif defined(__i386__)
-    || defined(_M_IX86) 
-    || defined(__arm__) 
-    || defined(_M_ARM)
+#elif defined(__i386__) || defined(_M_IX86) || defined(__arm__) || defined(_M_ARM)
     typedef uint32_t arch_type;
 #else
-    #error "Unsupported architecture:
-            Only Intel (x86/x64) and ARM (32-bit/64-bit) 
-            architectures are supported"
+    #error "Unsupported architecture: Only Intel (x86/x64) and ARM (32-bit/64-bit) architectures are supported"
 #endif
 
 volatile static uint8_t HEAP_COUNT = 0;
@@ -27,8 +19,7 @@ void* c_global_allocator(arch_type bytes) {
     if (ptr != NULL) {
         HEAP_COUNT++;
     }
-    /* return no matter if null required 
-    for handle_alloc_error in rust */
+    /* Return the pointer even if it's NULL to let Rust handle allocation errors */
     return ptr;
 }
 
