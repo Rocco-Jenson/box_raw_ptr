7281f8d7ac1d00828d0d96e79489c555d7c26c16
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-15 11:55:45 -0700

    Update allocator.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -2,17 +2,17 @@ use std::alloc::{GlobalAlloc, Layout, handle_alloc_error};
 
 /* C Types to remove libc dependency */
 #[cfg(target_pointer_width = "64")]
-type arch_bytes = u64;
+type arch_type = u64;
 
 #[cfg(target_pointer_width = "32")]
-type arch_bytes = u32;
+type arch_type = u32;
 
 type c_void = std::ffi::c_void;
 
 extern "C" {
-    fn c_global_allocator(bytes: arch_bytes) -> *mut c_void;
+    fn c_global_allocator(bytes: arch_type) -> *mut c_void;
     fn c_global_deallocator(ptr: *mut c_void) -> c_void;
-    fn global_allocation_info() -> arch_bytes; 
+    fn global_allocation_info() -> arch_type; 
 }
 
 #[allow(non_camel_case_types)]
@@ -20,8 +20,8 @@ struct C_GLOBAL_ALLOCATOR;
 
 unsafe impl GlobalAlloc for C_GLOBAL_ALLOCATOR {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let ptr: *mut u8 = c_global_allocator(layout.size()) as *mut u8;
-        let heap_count: arch_bytes = global_allocation_info();
+        let ptr: *mut u8 = c_global_allocator(layout.size() as arch_type) as *mut u8;
+        let heap_count: arch_type = global_allocation_info();
         
         if ptr.is_null() || heap_count < 0 {
             handle_alloc_error(layout);
