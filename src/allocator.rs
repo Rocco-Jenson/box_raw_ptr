cc1589021d172743df42d2af25a037128c6ad68d
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-15 11:50:05 -0700

    Update allocator.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -12,6 +12,7 @@ type c_void = std::ffi::c_void;
 extern "C" {
     fn c_global_allocator(bytes: arch_bytes) -> *mut c_void;
     fn c_global_deallocator(ptr: *mut c_void) -> c_void;
+    fn global_allocation_info() -> arch_bytes; 
 }
 
 #[allow(non_camel_case_types)]
@@ -20,14 +21,17 @@ struct C_GLOBAL_ALLOCATOR;
 unsafe impl GlobalAlloc for C_GLOBAL_ALLOCATOR {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let ptr: *mut u8 = c_global_allocator(layout.size()) as *mut u8;
-        if ptr.is_null() {
+        let heap_count: arch_bytes = global_allocation_info();
+        
+        if ptr.is_null() || heap_count < 0 {
             handle_alloc_error(layout);
         }
+        
         ptr
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        // ptr null check is done in function
+        /* Ptr null check is done in function */
         c_global_deallocator(ptr as *mut c_void);
     }
 }
