b5136900f828351e8fd1d19ad19c447397a5ccde
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-16 00:35:10 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,5 @@
 fn main() {
-    /*
-    Build allocator.c into a
-    .dll (dynamic link library) file
-    */
     cc::Build::new()
         .file("src/allocator.c")
-        .shared_flag(true)
         .compile("allocator")
 }
