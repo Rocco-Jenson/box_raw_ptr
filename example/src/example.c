ad1dd39b516ebb3aba6dbecaabd6af9929d47c6a
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-16 01:58:38 -0700

    Update example.c

--- example/src/example.c
+++ example/src/example.c
@@ -4,9 +4,9 @@ int* get_c_ptr() {
     // Make memory allocation of 1 int
     int* ptr = (int*)malloc(sizeof(int));
     // Check if memory allocation failed
-    int* val = (ptr != NULL) ? ptr : NULL;
+    if (ptr == NULL) { exit(EXIT_FAILURE); }
     // Assign value to int* ptr
-    *val = 12;
+    *ptr = 12;
     // Return ptr with assigned value
-    return val;
+    return ptr;
 }
