ee0784cf22a6045593ef4bc41fb6a5e9589a7e07
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-06-16 01:06:20 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
 fn main() {
+    /*
+    Compiles a static library from allocator.c file
+    NOTE: when using link attribute use
+    'kind' parameter to state that file is static 
+    or else linking errors will occur
+    example: #[link(name = "example", kind = "static")]
+    */
     cc::Build::new()
         .file("src/allocator.c")
         .compile("allocator")
